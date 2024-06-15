20c412874d899dd2f51c55da703980d568197b5f
Himanshu090705 <himanshujagtap369@gmail.com>
2024-06-15 18:11:28 +0530

    Added basic git info

--- Day-3/git-github/git-github.js
+++ Day-3/git-github/git-github.js
@@ -1,3 +1,12 @@
 // Git -> Free and Open source (version control system -> tools that help to tracks changes in code)
 
 // Github -> is a website where we host repositories online
+
+// init - used to create a new git repo    // git init
+
+// to add intial project to git below code is used
+// git remote add origin <-link->
+// git remote -v (to verify remote)
+// git branch (to check branch)
+// git branch -M main (to rename branch)
+// git push origin main
